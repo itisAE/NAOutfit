123d4e2a955aa2b48813fba8ac52d946a6e2596c
itisAE <eugenio.armando@itiscuneo.eu>
2024-01-30 21:22:57 +0100

    versione base OK

--- Server/server.py
+++ Server/server.py
@@ -1,17 +1,23 @@
-# Python 3
+# Server in python 3
 import socket
 import time
 
-# Creare un socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+NUM_B_RECV=2048								#numero di byte da ricevere a volta
+server_address = ('192.168.1.97', 22222)	# Assegnare l'indirizzo IP e la porta
 
-# Assegnare l'indirizzo IP e la porta
-server_address = ('192.168.168.100', 22222)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(server_address)
 
-# Ascoltare le connessioni in arrivo
 server_socket.listen(1)
 print("Server pronto")
+
+def invia(str, temp):
+    client_socket.sendall(str.encode())    #sendall al posto che send manda tutti i dati presenti nel buffer, invece send normale solo il numero di byte indicati ogni volta
+    time.sleep(temp)	#tempo che impiega il nao per parlare (si potrebbe fare parte client facendo mandare al server quando a terminato)
+def ricevi():			#bloccante fino a quando nn riceve i dati
+    data = client_socket.recv(NUM_B_RECV)
+    return data.decode()
+
 while True:
     try:
         # Accetta le connessioni
@@ -19,42 +25,34 @@ while True:
 
         while True:
             try:
-                
-                data = client_socket.recv(2048)
+                data=ricevi()
                 if not data:
                     print('Il client si è disconnesso')
                     break
-                print('ricevuto "{}"'.format(data))
-
+                print('ricevuto "{}", quindi connessione stabilita'.format(data))
                 # Risponde
-                response = 'Per favore mettiti in posizione davanti alla fotocamera, così potrò capire le tue taglie'
-                client_socket.sendall(response.encode())    #sendall al posto che send manda tutti i dati presenti nel buffer, invece send normale solo il numero di byte indicati ogni volta
-                time.sleep(10)   #aspetta 5 secondi
-
+                invia('Per favore mettiti in posizione davanti alla fotocamera, così potrò capire le tue taglie',10)
+                
                 #TODO ___________far partire i programmi per la misura__________
                 
-                response = 'Ho trovato che per la maglia ti sta bene la taglia M, vuoi una taglia in più?'
-                client_socket.sendall(response.encode())
-                data = client_socket.recv(2048)
-                rx=data.decode()
-                print("Risposta alla domanda dal client: "+rx)
+                
+                invia('Ho trovato che per la maglia ti sta bene la taglia M, vuoi una taglia in più?',0)
+                rx=ricevi()
+                print("Risposta alla domanda dal client: " + rx)
                                   
-                
                 if rx=="si":
-                    response = 'Ho aumentato di una taglia'
-                    client_socket.sendall(response.encode())
+                    invia('Ho aumentato di una taglia', 10)
                 else:
-                    response = 'Hai confermato la taglia'
-                    client_socket.sendall(response.encode())
-                
-                time.sleep(10)
+                    invia('Hai confermato la taglia', 10)
+
 
             except socket.error as e:
                 print("Errore durante la comunicazione con il client: {}".format(e))    #stampa il tipo di errore che ha riscontrato
                 break
-
-        # Pulire la connessione
-        client_socket.close()
-
+        client_socket.close()  
     except socket.error as e:
         print("Errore di connessione: {}".format(e))
+
+
+
+        
